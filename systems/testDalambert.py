607c126fc65ad62ef260ecad306a61b13f4b15d0
Evan Kimpton <kimptonevan@gmail.com>
2024-02-23 13:07:31 -0500

    Convert down no. % -> int

--- systems/testDalambert.py
+++ systems/testDalambert.py
@@ -1,33 +1,30 @@
 from spin import spin
 from genStats import register
 
-def testDalambert(downAmount, spins, startingBet, start, twelve = '2nd12'):
-    old_balance = start; balance = start
-    registerHold = []
+def testDalambert(downAmount = 1, spins = 100, baseBet = 10, balance = 1000, twelve = '2nd12'):
+    old_balance = balance
     max_balance = balance
-    won = 0
-    bet = startingBet
+    registerHold = []
+    bet = baseBet
     down = downAmount
     for i in range(1, spins+1):
         balance = spin(balance, bet, twelve)[0]
-        if balance < 10*down:
+        if balance < baseBet*down:
             break
 
-        # If you won, subtract 10 from bet
+        # If you won, decrease bet
         if balance > old_balance:
-            # reduce bet by 10-30% if won
-            bet -= int(bet*0.1*down) if bet > 10*down else 0 # if not back to start, subtract ten * down amount
-            (10*down) if bet > (10*down) else 0 # if not back to start, add ten * down amount
+            bet -= baseBet*down 
+            if bet < baseBet*down: bet = baseBet*down
             won = 1
 
-        # If you lost, increase bet by 10-30%
+        # If you lost, increase bet
         elif balance < old_balance:
-            bet += int(bet*0.1*down)
-            if bet >= balance:
-                bet = 10*down
+            bet += baseBet*down
+            if bet >= balance: bet = baseBet*down
             won = 0
-        max_balance = max(max_balance, balance)
-        registerHold.append([i, balance, won, max_balance]) # Spin no.,  balance, win/lose, max
+        
+        registerHold.append([i, balance, won, max(max_balance, balance)]) # Spin no., balance, win/lose, max
         old_balance = balance
     
     # Add simulation details to register
