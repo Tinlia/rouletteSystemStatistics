11e98bf61db85135a9f7e11a95f5ff13dc8696c6
Evan Kimpton <kimptonevan@gmail.com>
2024-02-23 13:08:33 -0500

    Improve Readability

--- systems/testEvans.py
+++ systems/testEvans.py
@@ -11,21 +11,15 @@ def testEvans(spins, baseBet, balance, num = '1'):
     for i in range(1, spins+1):
         numColor = squares[num][0]
         bet = 0 if numColor == previousColour else baseBet # Bet only when the colour changes
+        
         if numColor != previousColour:
             previousColour = numColor
         [balance, num] = spin(balance, bet, 'red' if squares[num][0] == 'black' else 'black')
         
-        if balance < baseBet:
-            break
-
-        # If you won
-        if balance > old_balance:
-            won = 1
-        # If you lost
-        elif balance < old_balance:
-            won = 0
-        else:
-            won = 0.5
+        if balance < baseBet:       break
+        if balance > old_balance:   won = 1
+        elif balance < old_balance: won = 0
+        else:                       won = 0.5
         
         max_balance = max(max_balance, balance)
         registerHold.append([i, balance, won, max_balance]) # Spin no., balance, win/lose
